4c5c20867daf3cfceb2a5386013fc0f16c1b5f97
Madhav1217 <madhava1219@gmail.com>
2019-08-08 13:32:36 +0530

    testfile update

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-aws codepipeline start-pipeline-execution --name fristpipeline >> fristpipeline.log 2>&1
-data=$(grep "pipelineExecutionId" fristpipeline.log)
+aws codepipeline start-pipeline-execution --name firstpipeline >> firstpipeline.log 2>&1
+data=$(grep "pipelineExecutionId" firstpipeline.log)
 pipelineid=$(echo "$data" | sed 's/.*| \(.*\)|/\1/')
-rm -rf fristpipeline.log
+rm -rf firstpipeline.log
 echo $pipelineid
 while true
 do
-   aws codepipeline get-pipeline-execution --pipeline-name fristpipeline --pipeline-execution-id $pipelineid >> status_data.log 2>&1
+   aws codepipeline get-pipeline-execution --pipeline-name firstpipeline --pipeline-execution-id $pipelineid >> status_data.log 2>&1
    cat status_data.log
    status_value=$(grep "InProgress" status_data.log)
    echo $status_value
